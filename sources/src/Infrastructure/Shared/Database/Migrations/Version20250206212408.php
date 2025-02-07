6f183c56fd0ed102b6e30e2af92975e9972234ea
amikha1lov <alexey.mikha1lov@yandex.ru>
2025-02-07 23:54:49 +0300

    fix: namespace migrations

--- sources/src/Infrastructure/Shared/Database/Migrations/Version20250206212408.php
+++ sources/src/Infrastructure/Shared/Database/Migrations/Version20250206212408.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Shared\Infrastructure\Database\Migrations;
+namespace App\Infrastructure\Shared\Database\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
